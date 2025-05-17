30d773d23d8f56a75fb09e5b6c5b86d42e1a7706
Chris McKenzie <kristopolous@yahoo.com>
2025-05-17 11:37:21 -0700

    version bump

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -eEuo pipefail
 version=$(grep version pyproject.toml  | cut -d '"' -f 2)
 tag_update() {
-    git tag -m v$version $version
+    git tag -m v$version v$version
     git push --tags
 }
 pipy() {
     source .venv/bin/activate
     for i in pip hatch build; do
-        pip install --upgrade $i
+        python3 -m pip install --upgrade $i
     done
     python3 -m build .
     twine upload dist/*${version}*
 }
-tag_update
+#tag_update
 pipy
